34afe694131883d5defe8bc66b67602a58229268
dennohkobe <dennohkobe@gmail.com>
2022-09-08 06:30:40 -0700

    printing

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,9 @@
-#!/bin/bash
-puts("\"Programming is like building a multilingual puzzle");
+#include <stdio.h>
+
+/*
+ *   Print "Programming is like building a multilingual puzzle"
+ */
+int main(void) {
+		puts("\"Programming is like building a multilingual puzzle");
+			return (0);
+		}		
