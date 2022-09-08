b54b6b49cb9da0a3304fd377f7abd5461c84c09e
dennohkobe <dennohkobe@gmail.com>
2022-09-08 15:27:59 -0700

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,8 +5,7 @@
   */
 int main()
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",);
-	sizeof("and that piece of art is useful\" - Dora Korpar, 2015-10-19"));
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",59);
 
 return (1);
 }
