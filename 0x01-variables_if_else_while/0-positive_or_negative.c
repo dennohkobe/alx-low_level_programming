5c9803d94cdd610e6c2c4ba61641b4b82738d526
dennohkobe <dennohkobe@gmail.com>
2022-09-10 03:55:02 -0700

    Random numbers positive negative

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,15 +1,15 @@
-/**
-* Positive or negative
-*/
+/*
+ * Code for 0-positive_or_negative.c
+ * Writter: DGK
+ */
 
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
 
 /**
-* Random numbers
-*/
-
+ * print positive, negative or zero.
+ */
 int main(void)
 {
 	int n;
@@ -17,14 +17,11 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 
-if (n > 0)
+	if (n > 0)
 	printf("%d is positive\n", n);
-
-else if (n < 0)
+	else if (n < 0)
 	printf("%d is negative\n", n);
-
-else
+	else
 	printf("%d is zero\n", n);
-
 	return (0);
 }
