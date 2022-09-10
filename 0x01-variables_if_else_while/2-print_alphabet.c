725212f603bf0f4637f540e6da2cd0c6904df722
dennohkobe <dennohkobe@gmail.com>
2022-09-10 06:52:35 -0700

    printing alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+/*
+ * Printing alphabet
+ */
+
+#include <stdio.h>
+
+/**
+ * main function description
+ */
+int main(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+	putchar(letter);
+	putchar('\n');
+	/* return desc */
+	return (0);
+}
