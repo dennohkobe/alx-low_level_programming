8f81fcbb2e288e6bda8f132727add75ab818b97d
dennohkobe <dennohkobe@gmail.com>
2022-09-15 07:11:27 -0700

    print putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ *  * main - printing _ putchar
+ * Return: 0 (Ok)
+ */
+
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
+	return (0);
+}
