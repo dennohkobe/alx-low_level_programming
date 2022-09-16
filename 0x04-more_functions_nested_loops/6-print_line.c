450e2d4f196c29336f8e13845daaad2e03fcd42f
dennohkobe <dennohkobe@gmail.com>
2022-09-16 14:48:05 -0700

    print line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_line - print straight line
+ * Return:0 
+ */
+
+void print_line(int n)
+{
+	while (n-- > 0)
+	{
+	putchar('_');
+	}
+	putchar('\n');
+}
