2603d8d9d5ecb959af9789fea79fd5a5166b9586
dennohkobe <dennohkobe@gmail.com>
2022-09-21 14:46:56 -0700

    lowercase to uppercase

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * string_toupper - convert string from lowercase to uppercase
+ * @s: string to convert
+ * Return: converted string
+ */
+char *string_toupper(char *s)
+{
+	int j;
+
+	for (j = 0; s[j] != '\0'; j++)
+	if (s[j] > 96 && s[j] < 123)
+	s[j] -= 32;
+	return (s);
+}
