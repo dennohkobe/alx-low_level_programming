04a40c8d569b2f799e0eb57cd9b62978228051ce
dennohkobe <dennohkobe@gmail.com>
2022-09-21 15:31:32 -0700

    translate string to ROT13

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,31 +1,18 @@
 #include "main.h"
 /**
- * print_number - print number
- * @n: int to print
- * Return: void
+ * rot13 - translate string to ROT13
+ * @s: input string to translate
+ * Return: ROT13 conversion
  */
-void print_number(int n)
+char *rot13(char *s)
 {
-	int copy, nth, size = 1, ones = n % 10;
+	int i, j;
+	char alphabet[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char rot_it[] =   "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
 
-	n /= 10;
-	copy = n;
-	if (ones < 0)
-	{
-	ones *= -1, copy *= -1, n *= -1;
-	_putchar('-');
-	}
-	if (copy > 0)
-	{
-	while (copy / 10 != 0)
-	copy /= 10, size *= 10;
-	while (size > 0)
-	{
-	nth = n / size;
-	_putchar('0' + nth);
-	n -= nth * size;
-	size /= 10;
-	}
-	}
-	_putchar('0' + ones);
+	for (i = 0; s[i] != '\0'; i++)
+	for (j = 0; j < 52; j++)
+	if (s[i] == alphabet[j])
+	s[i] = rot_it[j], j = 52;
+	return (s);
 }
