a15a3fe2c4b3523ca2360f65864bc7b6a6aaee0e
dennohkobe <dennohkobe@gmail.com>
2022-10-01 14:45:02 -0700

    add files

---
+++ 0x09-static_libraries/0-isupper.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ *_isupper - function that checks uppercase
+ *@c: input
+ *Return: returns 1 if it is uppercase , 0 if not
+ */
+
+int _isupper(int c)
+{
+	if ((c >= 'A') && (c <= 'Z'))
+	return (1);
+
+	return (0);
+}
